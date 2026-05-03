31ea7f558addbdb0d63929a4afc767a79d3d88ab
lucas-mansur <lucasmansur@hotmail.com.br>
2026-05-03 17:19:38 -0300

    feat: implementa o ponto geográfico do restaurante e da entrega para uso no Looker

--- curry_company_dbt/models/marts/fct_orders.sql
+++ curry_company_dbt/models/marts/fct_orders.sql
@@ -7,9 +7,22 @@ compute_metrics as (
         *,
         -- Calculando a distância em KM usando Haversine (Função do BigQuery)
         round(st_distance(
-            st_geogpoint(restaurant_longitude, restaurant_latitude),
-            st_geogpoint(delivery_location_longitude, delivery_location_latitude)
-        ) / 1000, 2) as distance_km
+        st_geogpoint(restaurant_longitude, restaurant_latitude),
+        st_geogpoint(delivery_location_longitude, delivery_location_latitude)
+        ) / 1000, 2) as distance_km,
+
+
+
+        -- Criando o ponto geográfico do Restaurante
+        ST_GEOGPOINT(restaurant_longitude, restaurant_latitude) AS restaurant_geo_location,
+
+        -- Criando o ponto geográfico da Entrega
+        ST_GEOGPOINT(delivery_location_longitude, delivery_location_latitude) AS delivery_geo_location,
+
+  
+
+
+
     from orders
 )
 
