f47203d815073594a79cbc979eb310d13b82e1b3
lucas-mansur <lucasmansur@hotmail.com.br>
2026-05-03 18:51:52 -0300

    feat: ajuste do tipo dado de geolocalização aceito pelo looker, diferente do BQ

--- curry_company_dbt/models/marts/fct_orders.sql
+++ curry_company_dbt/models/marts/fct_orders.sql
@@ -5,23 +5,28 @@ with orders as (
 compute_metrics as (
     select
         *,
-        -- Calculando a distância em KM usando Haversine (Função do BigQuery)
+        -- 1. Calculando a distância exata entre os pontos (Em KM)
+        -- Usamos ST_DISTANCE que é o padrão de ouro para cálculos geoespaciais
         round(st_distance(
-        st_geogpoint(restaurant_longitude, restaurant_latitude),
-        st_geogpoint(delivery_location_longitude, delivery_location_latitude)
+            st_geogpoint(restaurant_longitude, restaurant_latitude),
+            st_geogpoint(delivery_location_longitude, delivery_location_latitude)
         ) / 1000, 2) as distance_km,
 
+        -- 2. Criando a coordenada do Restaurante para o Looker
+        -- Formato esperado pelo Looker: "Lat,Long" (String)
+        concat(
+            safe_cast(restaurant_latitude as string), 
+            ",", 
+            safe_cast(restaurant_longitude as string)
+        ) as restaurant_geo_location,
 
-
-        -- Criando o ponto geográfico do Restaurante
-        ST_GEOGPOINT(restaurant_longitude, restaurant_latitude) AS restaurant_geo_location,
-
-        -- Criando o ponto geográfico da Entrega
-        ST_GEOGPOINT(delivery_location_longitude, delivery_location_latitude) AS delivery_geo_location,
-
-  
-
-
+        -- 3. Criando a coordenada da Entrega para o Looker
+        -- Formato esperado pelo Looker: "Lat,Long" (String)
+        concat(
+            safe_cast(delivery_location_latitude as string), 
+            ",", 
+            safe_cast(delivery_location_longitude as string)
+        ) as delivery_geo_location
 
     from orders
 )
